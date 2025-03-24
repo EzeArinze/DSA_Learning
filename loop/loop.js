acfe516a57483445da2edce827e4ae31c74bc423
Arinze-Eze <emmanuelrinz@gmail.com>
2025-03-24 11:35:44 +0100

    loop

---
+++ loop/loop.js
@@ -0,0 +1,22 @@
+//for loop (used when you know the number of iteration)
+// let learning = "loop";
+// for (let i = 0; i <= 4; i++) {
+//   console.log(learning.charAt(i));
+// }
+
+//Nested Loop
+
+// let star = "";
+// for (let i = 1; i <= 1; i++) {
+//   for (let j = 1; j <= 5; j++) {
+//     star += "*";
+//     console.log(star);
+//   }
+// }
+
+//while loop (used when you don't know the number of iteration)
+
+// let sum = 0;
+// while (sum <= 5) {
+//   console.log(sum++);
+// }
